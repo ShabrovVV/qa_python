99dd71a810da7877024708461ecffd417e554aeb
Vitaliy Shabrov <vvshabrov@yandex.ru>
2026-06-20 20:22:52 +0300

    add_new_book

--- tests.py
+++ tests.py
@@ -1,24 +1,38 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+# ========== добавляем новую книгу ==========
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+def test_add_new_book_name_more_than_40_symbols(self):
+        collector = BooksCollector()
+        long_name = 'А' * 41
+        collector.add_new_book(long_name)
+        assert len(collector.get_books_genre()) == 0
+
+    def test_add_new_book_empty_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('')
+        assert len(collector.get_books_genre()) == 0
+
+    @pytest.mark.parametrize('name', ['Книга 1', 'Книга 2', 'Книга с названием'])
+    def test_add_new_book_different_names(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name in collector.get_books_genre()
+        assert collector.get_book_genre(name) == ''
+
+    def test_add_new_book_duplicate_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга')
+        collector.add_new_book('Книга')
+        assert len(collector.get_books_genre()) == 1
+
